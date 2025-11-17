fb9e44f92d80ac1978f70fc99bfdda0bf4593f01
Il4ry <ilariatenchini@gmail.com>
2025-11-17 17:32:18 +0100

    blocco sincronizzato

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        Counter counter = new Counter();
+        Contatore counter = new Contatore();
 
         // Create multiple threads to increment the counter
         Thread t1 = new Thread(() -> counter.increment());
         Thread t2 = new Thread(() -> counter.increment());
+        SynchronizedBlockExample example = new SynchronizedBlockExample();
+        Thread t3 = new Thread(() -> example.increment());
+        Thread t4 = new Thread(() -> example.increment());
 
         t1.start();
         t2.start();
+        t3.start();
+        t4.start();
 
         try {
             t1.join();
             t2.join();
+            t3.join();
+            t4.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
